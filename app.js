d3dcc5c9d811690b28c165355f16b08195e20f53
Peter.O <ojakaapeter@gmail.com>
2017-12-16 15:58:03 +0300

    Set up express server and messenger side

--- app.js
+++ app.js
@@ -9,22 +9,138 @@ let Wit = require('node-wit').Wit;
 let log = require('node-wit').log;
 
 const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
-process.env.MESSENGER_APP_SECRET :
-config.get('appSecret');
+    process.env.MESSENGER_APP_SECRET :
+    config.get('appSecret');
 
 const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
-(process.env.MESSENGER_VALIDATION_TOKEN) :
-config.get('validationToken');
+    (process.env.MESSENGER_VALIDATION_TOKEN) :
+    config.get('validationToken');
 
 const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
-(process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
-config.get('pageAccessToken');
+    (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+    config.get('pageAccessToken');
 
 const WIT_TOKEN = (process.env.WIT_SERVER_ACCESS_TOKEN) ?
-(process.env.WIT_SERVER_ACCESS_TOKEN) :
-config.get('witToken');
+    (process.env.WIT_SERVER_ACCESS_TOKEN) :
+    config.get('witToken');
 
 if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && WIT_TOKEN)) {
-console.error("Missing config values");
-process.exit(1);
+    console.error("Missing config values");
+    process.exit(1);
 }
+
+var app = express();
+app.set('port', process.env.PORT || 5000);
+app.set('view engine', 'ejs');
+app.use(bodyParser.json({
+    verify: verifyRequestSignature
+}));
+
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+        console.log("Validating webhook");
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);
+    }
+});
+
+// Message handler
+app.post('/webhook', (req, res) => {
+    // Parse the Messenger payload
+    // See the Webhook reference
+    // https://developers.facebook.com/docs/messenger-platform/webhook-reference
+    const data = req.body;
+  
+    if (data.object === 'page') {
+      data.entry.forEach(entry => {
+        entry.messaging.forEach(event => {
+          if (event.message && !event.message.is_echo) {
+            // Yay! We got a new message!
+            // We retrieve the Facebook user ID of the sender
+            const sender = event.sender.id;
+  
+            // We retrieve the user's current session, or create one if it doesn't exist
+            // This is needed for our bot to figure out the conversation history
+            const sessionId = findOrCreateSession(sender);
+  
+            // We retrieve the message content
+            const {text, attachments} = event.message;
+  
+            if (attachments) {
+              // We received an attachment
+              // Let's reply with an automatic message
+              fbMessage(sender, 'Sorry I can only process text messages for now.')
+              .catch(console.error);
+            } else if (text) {
+              // We received a text message
+  
+              // Let's forward the message to the Wit.ai Bot Engine
+              // This will run all actions until our bot has nothing left to do
+              wit.runActions(
+                sessionId, // the user's current session
+                text, // the user's message
+                sessions[sessionId].context // the user's current session state
+              ).then((context) => {
+                // Our bot did everything it has to do.
+                // Now it's waiting for further messages to proceed.
+                console.log('Waiting for next user messages');
+  
+                // Based on the session state, you might want to reset the session.
+                // This depends heavily on the business logic of your bot.
+                // Example:
+                // if (context['done']) {
+                //   delete sessions[sessionId];
+                // }
+  
+                // Updating the user's current session state
+                sessions[sessionId].context = context;
+              })
+              .catch((err) => {
+                console.error('Oops! Got an error from Wit: ', err.stack || err);
+              })
+            }
+          } else {
+            console.log('received event', JSON.stringify(event));
+          }
+        });
+      });
+    }
+    res.sendStatus(200);
+  });
+  
+  /*
+   * Verify that the callback came from Facebook. Using the App Secret from
+   * the App Dashboard, we can verify the signature that is sent with each
+   * callback in the x-hub-signature field, located in the header.
+   *
+   * https://developers.facebook.com/docs/graph-api/webhooks#setup
+   *
+   */
+  function verifyRequestSignature(req, res, buf) {
+    var signature = req.headers["x-hub-signature"];
+  
+    if (!signature) {
+      // For testing, let's log an error. In production, you should throw an
+      // error.
+      console.error("Couldn't validate the signature.");
+    } else {
+      var elements = signature.split('=');
+      var method = elements[0];
+      var signatureHash = elements[1];
+  
+      var expectedHash = crypto.createHmac('sha1', FB_APP_SECRET)
+                          .update(buf)
+                          .digest('hex');
+  
+      if (signatureHash != expectedHash) {
+        throw new Error("Couldn't validate the request signature.");
+      }
+    }
+  }
+
+  app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+  });
