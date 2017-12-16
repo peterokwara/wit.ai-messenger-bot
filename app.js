5aa694f464182230878a3167848f2c591cf0791d
Peter.O <ojakaapeter@gmail.com>
2017-12-16 15:18:16 +0300

    Import the necessary config values from the config file

--- app.js
+++ app.js
@@ -8,4 +8,23 @@ const config = require('config');
 let Wit = require('node-wit').Wit;
 let log = require('node-wit').log;
 
-var app = express();
+const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
+process.env.MESSENGER_APP_SECRET :
+config.get('appSecret');
+
+const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+(process.env.MESSENGER_VALIDATION_TOKEN) :
+config.get('validationToken');
+
+const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+(process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+config.get('pageAccessToken');
+
+const SERVER_URL = (process.env.SERVER_URL) ?
+(process.env.SERVER_URL) :
+config.get('serverURL');
+
+if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
+console.error("Missing config values");
+process.exit(1);
+}
