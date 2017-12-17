3bccc8b7ef232761c2ada24301c67dbb0d8dad2e
Peter.O <ojakaapeter@gmail.com>
2017-12-17 13:52:35 +0300

    Add facebook code

--- app.js
+++ app.js
@@ -48,3 +48,111 @@ function firstEntity(nlp, name) {
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
+
+function receivedMessage(event) {
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
+
+    console.log("Received message for user %d and page %d at %d with message:",
+        senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
+
+    var messageId = message.mid;
+
+    // You may get a text or attachment but not both
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
+    var quickReply = message.quick_reply;
+
+}
+
+// Send a receipt at the end of the transaction
+function sendMessage(recipientId) {
+
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            "text": "Here is your receipt"
+        }
+    };
+
+    callSendAPI(messageData);
+}
+
+// Sending messages
+function callSendAPI(messageData) {
+    return new Promise((resolve, reject) => {
+        request({
+            uri: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: {
+                access_token: PAGE_ACCESS_TOKEN
+            },
+            method: 'POST',
+            json: messageData,
+        }, (error, response, body) => {
+            if (!error && response.statusCode == 200) {
+                const recipientId = body.recipient_id;
+                const messageId = body.message_id;
+                console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
+                resolve();
+            } else {
+                console.error("Unable to send message.");
+                console.error(body);
+                console.error(error);
+                reject();
+            }
+        });
+    });
+};
+
+
+function verifyRequestSignature(req, res, buf) {
+    var signature = req.headers["x-hub-signature"];
+
+    if (!signature) {
+        // For testing, let's log an error. In production, you should throw an
+        // error.
+        console.error("Couldn't validate the signature.");
+    } else {
+        var elements = signature.split('=');
+        var method = elements[0];
+        var signatureHash = elements[1];
+
+        var expectedHash = crypto.createHmac('sha1', APP_SECRET)
+            .update(buf)
+            .digest('hex');
+
+        if (signatureHash != expectedHash) {
+            throw new Error("Couldn't validate the request signature.");
+        }
+    }
+};
+
+app.post('/webhook', function (req, res) {
+    var data = req.body;
+    var orderSubtitle;
+
+    // Make sure this is a page subscription
+    if (data.object == 'page') {
+        // Iterate over each entry
+        // There may be multiple if batched
+        data.entry.forEach(function (pageEntry) {
+            var pageID = pageEntry.id;
+            var timeOfEvent = pageEntry.time;
+
+            // Iterate over each messaging event
+            pageEntry.messaging.forEach(function (messagingEvent) {
+                if (messagingEvent.message) {
+                    receivedMessage(messagingEvent);
+                } else  {
+                    console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+                }
+            });
+        });
+        res.sendStatus(200);
+    }
+});
